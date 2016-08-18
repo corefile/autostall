a333a1894329e903b7510606051214a544545f49
Paul Huckaby <luap71@yahoo.com>
2016-08-18 15:34:37 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -10,9 +10,28 @@ if ! command -v brew >/dev/null; then
   echo "Installing Homebrew ..."
     curl -fsS \
       'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
- export PATH="/usr/local/bin:$PATH"
+
 fi
+
+export PATH="/usr/local/bin:$PATH"
 
 echo "Upda-grading Homebrew..."
   successfully brew update
   successfully brew upgrade
+  
+################################################################[ SW inst ]#####
+echo "Hipsterizing your terminal with homebrewed zsh"
+  successfully brew install zsh
+  successfully brew install zsh-completions
+  successfully brew install zsh-syntax-highlighting
+ 
+  
+  successfully brew install httpie
+  successfully brew install wget
+  successfully brew install mtr
+  successfully brew install git
+  successfully brew install vim
+  successfully brew install tmux
+  successfully brew install imagemagick
+  successfully brew install youtube-dl
+  successfully brew install watch
